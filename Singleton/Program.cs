72387741c19230b2b89c4122d7678fc3f3c5ce38
ar13101085 <ar13101085@gmail.com>
2018-08-28 14:45:57 +0600

    implement single ton pattern..

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonImageViewer imageViewer1=SingletonImageViewer.GetInstance;
+            SingletonImageViewer imageViewer2=SingletonImageViewer.GetInstance;
+            
+          //  Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/SingletonImageViewer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class SingletonImageViewer //sealed means inherited class can't call parrent constructor multiple time
+    {
+        private static int count = 0;
+        private static SingletonImageViewer Instance { get; set; }
+        private static Object obj=new object();
+        
+        public static SingletonImageViewer GetInstance
+        {
+            get
+            {
+                //Single ton with thread save implementation
+                if (Instance == null) //implement double lock checking for lock optimization checking
+                {
+                    lock (obj)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance=new SingletonImageViewer();
+                        }
+                    }
+                }
+                
+
+                return Instance;
+
+            }
+        }
+
+        private SingletonImageViewer()
+        {
+            Console.WriteLine("contructor call");
+            Console.WriteLine(++count+"");
+        }
+    }
+}
